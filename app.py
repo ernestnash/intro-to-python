0e8427230c3df079f1c6275d74a6ba96a1770fb3
ernestnash <eernestnnashon@gmail.com>
2022-08-20 23:06:23 +0300

    lesson 1

---
+++ app.py
@@ -0,0 +1,15 @@
+# from math import *
+
+# # name = 'Tim'
+# # age = 18 
+# print(sqrt(100))
+
+name = 'jose'
+age = 48
+
+print(f'{name} is {age} yeas old.')#f-string(formated string literals)
+y = ('{} is {} yeas old.'.format(name,age))#format() method
+
+
+
+print(y.split())
